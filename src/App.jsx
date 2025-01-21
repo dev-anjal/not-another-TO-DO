9e6a25e3bbfa08fcdccf615e572ed5532a2916f3
Flame Anjal <kinganjalap@gmail.com>
2025-01-21 20:41:43 +0545

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Home() {
+  const [NewItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem("todos")) || []
+  );
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos([
+      ...todos,
+      { id: crypto.randomUUID(), text: NewItem, completed: false },
+    ]);
+    setNewItem("");
+  }
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos, completeTask]);
+
+  function completeTask(key) {
+    const newTodos = todos.map((todo) =>
+      todo.id === key ? { ...todo, completed: !todo.completed } : todo
+    );
+    console.log(newTodos);
+
+    setTodos(newTodos);
+  }
+  function deletetodo(key) {
+    const newTodos = todos.filter((todo) => todo.id !== key);
+    setTodos(newTodos);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className="form-row" onSubmit={handleSubmit}>
+        <label htmlFor="item">New Item</label>
+        <input
+          type="text"
+          id="item"
+          value={NewItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To DO list</h1>
+      <ul className="list">
+        {todos.map((todo, id) => (
+          <li key={id} className={id.completed ? "completed" : ""}>
+            <label>
+              <input
+                type="checkbox"
+                {...(todo.completed ? { checked: true } : {})}
+                name=""
+                id=""
+                onClick={() => completeTask(todo.id)}
+              />
+              {todo.text}
+            </label>
+            <button
+              className="btn btn-danger"
+              onClick={() => deletetodo(todo.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
